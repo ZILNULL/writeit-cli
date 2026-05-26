38200a3697482ad2c1cf60de4aa697d4e8e31334
ZILNULL <zilnullart@gmail.com>
2026-05-26 11:38:30 +0200

    Refactored layout, added ability to shift editors.

--- src/editor.py
+++ src/editor.py
@@ -1,10 +1,10 @@
 from textual import on
 from textual.app import ComposeResult
 from textual.binding import Binding
-from textual.containers import Horizontal, VerticalScroll
 from textual.widget import Widget
 from textual.widgets import Input
 
+from src.editor_layout import EditorLayout
 from src.footer import FooterWI
 from src.header import HeaderWI
 from src.textarea import TextAreaExt
@@ -15,87 +15,221 @@ class Editor(Widget, can_focus=True):
     BINDINGS = [
         Binding("escape", "normal_mode"),
         Binding("i", "insert_mode"),
-        Binding("h", "move_left"),
-        Binding("j", "move_down"),
-        Binding("k", "move_up"),
-        Binding("l", "move_right"),
+        Binding("h", "move_cursor('h')"),
+        Binding("j", "move_cursor('j')"),
+        Binding("k", "move_cursor('k')"),
+        Binding("l", "move_cursor('l')"),
         Binding(":", "command_mode"),
         Binding("enter", "command_submit"),
         Binding("a", "new_text_area"),
+        Binding("ctrl+h", "move_to_left_area"),
+        Binding("ctrl+j", "move_to_down_area"),
+        Binding("ctrl+k", "move_to_up_area", priority=True),
+        Binding("ctrl+l", "move_to_right_area"),
+        Binding("alt+h", "shift_to_left_area", priority=True),
+        Binding("alt+j", "shift_to_down_area", priority=True),
+        Binding("alt+k", "shift_to_up_area", priority=True),
+        Binding("alt+l", "shift_to_right_area", priority=True),
     ]
-
-    DEFAULT_CSS = """
-    VerticalScroll {
-        display: none;
-    }
-
-    VerticalScroll TextAreaExt {
-        height: 50%;
-    }
-    """
 
     def __init__(self, id: str | None = None):
         super().__init__(id=id)
         self.mode = "n"
         self.current_text_area = 0
-        self.edit_position = None
+        self.previous_text_area = None
+
         self.editor_header = HeaderWI()
         self.text_areas = [TextAreaExt(id="input0")]
         self.editor_footer = FooterWI()
-        self.vertical_container = VerticalScroll()
-        self.horizontal_container = Horizontal(
-            self.text_areas[0],
-            self.vertical_container,
-        )
+        self.horizontal_container = EditorLayout(self.text_areas)
 
     def compose(self) -> ComposeResult:
         yield self.editor_header
         yield self.horizontal_container
         yield self.editor_footer
 
+    async def rebuild_containers(self) -> None:
+        await self.horizontal_container.update_content(self.text_areas)
+
+    def focus_area(self, index: int) -> None:
+        if len(self.text_areas) == 0:
+            return
+
+        self.current_text_area = index
+        self.text_areas[index].read_only = True
+        self.text_areas[index].focus()
+
+    def unfocus_area(self, index: int | None = None) -> None:
+        if len(self.text_areas) == 0:
+            return
+
+        index = self.current_text_area if index is None else index
+        self.text_areas[index].read_only = True
+
+    def insert_mode_area(self, index: int | None = None) -> None:
+        if len(self.text_areas) == 0:
+            return
+
+        if index is not None:
+            self.focus_area(index)
+
+        index = self.current_text_area
+        self.text_areas[index].read_only = False
+
+    def change_mode(self, new_mode: str) -> bool:
+        if len(self.text_areas) == 0 and new_mode == "i":
+            return False
+
+        self.mode = new_mode
+        return True
+
+    # -------------------------------
+    # Actions
+    # -------------------------------
+    def check_action(self, action: str, parameters: tuple[object, ...]) -> bool | None:
+        if len(self.text_areas) == 0:
+            not_allowed = [
+                "insert_mode",
+                "move_cursor",
+                "move_to_left_area",
+                "move_to_right_area",
+                "move_to_up_area",
+                "move_to_down_area",
+                "shift_to_left_area",
+                "shift_to_right_area",
+                "shift_to_up_area",
+                "shift_to_down_area",
+            ]
+            if action in not_allowed:
+                return False
+        return True
+
     def action_normal_mode(self) -> None:
-        currTextArea = self.text_areas[self.current_text_area]
-        currTextArea.read_only = True
-        currTextArea.focus()
-        self.mode = "n"
+        self.focus_area(self.current_text_area)
+        self.change_mode("n")
 
     def action_insert_mode(self) -> None:
-        currTextArea = self.text_areas[self.current_text_area]
-        currTextArea.read_only = False
-        self.mode = "i"
+        self.insert_mode_area()
+        self.change_mode("i")
 
     def action_command_mode(self) -> None:
         commandWindow = self.editor_footer.command_input
         commandWindow.focus()
-        self.mode = "c"
+        self.change_mode("c")
 
-    def action_move_left(self) -> None:
-        currTextArea = self.text_areas[self.current_text_area]
-        currTextArea.move_cursor_relative(0, -1, record_width=True)
-
-    def action_move_right(self) -> None:
-        currTextArea = self.text_areas[self.current_text_area]
-        currTextArea.move_cursor_relative(0, 1, record_width=True)
-
-    def action_move_down(self) -> None:
-        currTextArea = self.text_areas[self.current_text_area]
-        currTextArea.move_cursor_relative(1, 0, record_width=True)
-
-    def action_move_up(self) -> None:
-        currTextArea = self.text_areas[self.current_text_area]
-        currTextArea.move_cursor_relative(-1, 0, record_width=True)
-
-    def action_new_text_area(self) -> None:
+    async def action_new_text_area(self) -> None:
         new_input = len(self.text_areas)
         self.text_areas.append(TextAreaExt(id=f"input{new_input}"))
-        self.vertical_container.mount(self.text_areas[new_input])
-        self.current_text_area = new_input
-        self.text_areas[self.current_text_area].focus()
-        self.text_areas[self.current_text_area].read_only = True
+        await self.horizontal_container.update_content(self.text_areas)
+        self.focus_area(new_input)
 
-        if self.vertical_container.styles.display == "none":
-            self.vertical_container.styles.display = "block"
+    def action_move_cursor(self, key: str) -> None:
+        currTextArea = self.text_areas[self.current_text_area]
+        match key:
+            case "h":
+                currTextArea.move_cursor_relative(0, -1)
+            case "j":
+                currTextArea.move_cursor_relative(1, 0, record_width=True)
+            case "k":
+                currTextArea.move_cursor_relative(-1, 0, record_width=True)
+            case "l":
+                currTextArea.move_cursor_relative(0, 1)
+            case _:
+                raise ValueError("Wrong key passed to action_move_cursor.")
 
+    def action_move_to_left_area(self) -> None:
+        if self.current_text_area == 0:
+            return
+
+        self.previous_text_area = self.current_text_area
+        self.unfocus_area()
+        self.focus_area(0)
+
+    def action_move_to_right_area(self) -> None:
+        if self.current_text_area != 0 or len(self.text_areas) == 1:
+            return
+
+        next_area = (
+            self.previous_text_area if self.previous_text_area is not None else 1
+        )
+        self.previous_text_area = None
+        self.unfocus_area()
+        self.focus_area(next_area)
+
+    def action_move_to_up_area(self) -> None:
+        if self.current_text_area == 1 or self.current_text_area == 0:
+            return
+
+        self.unfocus_area()
+        self.focus_area(self.current_text_area - 1)
+
+    def action_move_to_down_area(self) -> None:
+        if (
+            self.current_text_area == len(self.text_areas) - 1
+            or self.current_text_area == 0
+        ):
+            return
+
+        self.unfocus_area()
+        self.focus_area(self.current_text_area + 1)
+
+    async def action_shift_to_left_area(self) -> None:
+        if self.current_text_area == 0:
+            return
+
+        curr = self.current_text_area
+        self.text_areas[0], self.text_areas[curr] = (
+            self.text_areas[curr],
+            self.text_areas[0],
+        )
+        await self.horizontal_container.update_content(self.text_areas)
+        self.action_move_to_left_area()
+
+    async def action_shift_to_right_area(self) -> None:
+        if self.current_text_area != 0 or len(self.text_areas) == 1:
+            return
+
+        next_area = (
+            self.previous_text_area if self.previous_text_area is not None else 1
+        )
+        self.text_areas[0], self.text_areas[next_area] = (
+            self.text_areas[next_area],
+            self.text_areas[0],
+        )
+        await self.horizontal_container.update_content(self.text_areas)
+        self.action_move_to_right_area()
+
+    async def action_shift_to_up_area(self) -> None:
+        if self.current_text_area == 1 or self.current_text_area == 0:
+            return
+
+        curr = self.current_text_area
+
+        self.text_areas[curr - 1], self.text_areas[curr] = (
+            self.text_areas[curr],
+            self.text_areas[curr - 1],
+        )
+        await self.horizontal_container.update_content(self.text_areas)
+        self.action_move_to_up_area()
+
+    async def action_shift_to_down_area(self) -> None:
+        if (
+            self.current_text_area == len(self.text_areas) - 1
+            or self.current_text_area == 0
+        ):
+            return
+
+        curr = self.current_text_area
+        self.text_areas[curr + 1], self.text_areas[curr] = (
+            self.text_areas[curr],
+            self.text_areas[curr + 1],
+        )
+        await self.horizontal_container.update_content(self.text_areas)
+        self.action_move_to_down_area()
+
+    # -------------------------------
+    # Commands
+    # -------------------------------
     @on(Input.Submitted)
     def action_command_submit(self) -> None:
         if self.mode != "c":
---
+++ src/editor_layout.py
@@ -0,0 +1,52 @@
+from textual.app import ComposeResult
+from textual.containers import Horizontal, VerticalScroll
+from textual.widget import Widget
+from textual.widgets import Label
+
+
+class EditorLayout(Widget, can_focus=True):
+    DEFAULT_CSS = """
+    VerticalScroll {
+        display: none;
+    }
+
+    VerticalScroll TextAreaExt {
+        height: 50%;
+    }
+    """
+
+    def __init__(self, text_areas: list[Widget] = []):
+        super().__init__()
+        self.text_areas = text_areas
+        self.sub_editors = VerticalScroll()
+        self.main_editor = Label("Editor goes here...")
+
+        if len(self.text_areas) == 1:
+            self.main_editor = self.text_areas[0]
+
+        if len(self.text_areas) > 1:
+            self.main_editor = self.text_areas[0]
+            self.sub_editors = VerticalScroll(*self.text_areas[1:])
+
+        self.layout_container = Horizontal(self.main_editor, self.sub_editors)
+
+    def compose(self) -> ComposeResult:
+        yield self.layout_container
+
+    async def update_content(self, text_areas: list[Widget]):
+        self.text_areas = text_areas
+        if len(self.text_areas) == 1:
+            self.main_editor = self.text_areas[0]
+            self.sub_editors.styles.display = "none"
+
+        if len(self.text_areas) > 1:
+            self.main_editor = self.text_areas[0]
+            self.sub_editors = VerticalScroll(*self.text_areas[1:])
+            self.sub_editors.styles.display = "block"
+
+        self.layout_container = Horizontal(self.main_editor, self.sub_editors)
+        await self.recompose()
+
+
+if __name__ == "__main__":
+    editor_layout = EditorLayout()
